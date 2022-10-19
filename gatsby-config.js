8291e1b1b8198ed797292d7a607bbe95ff8302f0
zhuyaguang <zhuyaguang1368@163.com>
2022-10-19 21:04:59 +0800

    add per

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/running', // Change to `/running_page` when running on github pages
+  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: '朱亚光爱运动',
     siteUrl: 'https://zhuyaguang.github.io/',
